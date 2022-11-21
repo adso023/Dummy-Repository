5b775771debf77d5ad3b842d98b6aba3da500aa0
Adrian D'Souza <dsouzaadrian24@gmail.com>
2022-11-21 10:19:14 -0500

    third commit

--- index.ts
+++ index.ts
@@ -3,11 +3,24 @@ import path from 'path';
 
 (() => {
 	console.log('Starting loop');
-	for (let i = 0; i < 10000000; i++) {
+	for (let i = 0; i < 10000; i++) {
 		fs.appendFileSync(
-			path.join(__dirname, 'src', 'test2.ts'),
+			path.join(__dirname, 'src', 'test1.ts'),
 			'console.log("Hello, World!");',
 		);
+		fs.appendFileSync(
+			path.join(__dirname, 'src', 'test2.ts'),
+			'console.log("Hello, World!");'
+		);
+		fs.appendFileSync(
+			path.join(__dirname, 'src', 'test3.ts'),
+			'console.log("Hello, World!");'
+		);
+		fs.appendFileSync(
+			path.join(__dirname, 'src', 'test4.ts'),
+			'console.log("Hello, World!");'
+		);
 	}
+
 	console.log('Loop completed');
 })();
