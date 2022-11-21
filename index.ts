f71d5a17daf093d9b9b168a302470988aa8f695d
adso023 <dsouzaadrian24@gmail.com>
2022-11-21 10:39:19 -0500

    Testing

--- index.ts
+++ index.ts
@@ -2,37 +2,37 @@ import fs from 'fs';
 import path from 'path';
 
 (() => {
-	console.log('Starting loop');
+  console.log('Starting loop');
 
-	for (let i = 0; i < 100; i++) {
-		console.log(`Writing to test${i}.ts`);
-		for(let j = 0; i < 1000; j++) {
-			fs.appendFileSync(
-				path.join(__dirname, 'src', `test${i}.ts`),
-				'console.log("Hello, World!");'
-			);
-		}
-		console.log(`Finished writing to test${i}.ts`);
-	}
+  for (let i = 0; i < 100; i++) {
+    console.log(`Writing to test${i}.ts`);
+    for (let j = 0; j < 600; j++) {
+      fs.appendFileSync(
+        path.join(__dirname, 'src', `test${i}.ts`),
+        'console.log("Hello, World!");',
+      );
+    }
+    console.log(`Finished writing to test${i}.ts`);
+  }
 
-	// for (let i = 0; i < 1000; i++) {
-	// 	fs.appendFileSync(
-	// 		path.join(__dirname, 'src', 'test1.ts'),
-	// 		'console.log("Hello, World!");',
-	// 	);
-	// 	fs.appendFileSync(
-	// 		path.join(__dirname, 'src', 'test2.ts'),
-	// 		'console.log("Hello, World!");'
-	// 	);
-	// 	fs.appendFileSync(
-	// 		path.join(__dirname, 'src', 'test3.ts'),
-	// 		'console.log("Hello, World!");'
-	// 	);
-	// 	fs.appendFileSync(
-	// 		path.join(__dirname, 'src', 'test4.ts'),
-	// 		'console.log("Hello, World!");'
-	// 	);
-	// }
+  // for (let i = 0; i < 1000; i++) {
+  // 	fs.appendFileSync(
+  // 		path.join(__dirname, 'src', 'test1.ts'),
+  // 		'console.log("Hello, World!");',
+  // 	);
+  // 	fs.appendFileSync(
+  // 		path.join(__dirname, 'src', 'test2.ts'),
+  // 		'console.log("Hello, World!");'
+  // 	);
+  // 	fs.appendFileSync(
+  // 		path.join(__dirname, 'src', 'test3.ts'),
+  // 		'console.log("Hello, World!");'
+  // 	);
+  // 	fs.appendFileSync(
+  // 		path.join(__dirname, 'src', 'test4.ts'),
+  // 		'console.log("Hello, World!");'
+  // 	);
+  // }
 
-	console.log('Loop completed');
+  console.log('Loop completed');
 })();
